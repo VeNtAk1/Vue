9b0e0e124225b8bc60ee13a7ed9ca90e03c96770
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-18 11:04:31 +0300

    8_Vue

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,11 @@ import NewComponent from './NewComponent.vue'
 export default{
   data() {
     return {
-      arr: ['x', 'y', 'z'],
-      result: [1, 2, 3]
+      obj: {x: 1, y: 2, z: 3},
     }
   }
 }
 </script>
 <template>
-  {{ arr[0] }}
-  {{ arr[1] }}
-  {{ arr[2] }}
-  <br>
-  {{ result[0] + result[1] + result[2] }}
+  {{ obj.x + obj.y + obj.z}}
 </template>
