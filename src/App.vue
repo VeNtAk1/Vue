33fff35b7db7dbbed4ccffd8f26031f4b752ca59
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-25 12:53:15 +0300

    19_3_Vue

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,25 @@ export default {
     data(){
         return{
             visible: false,
+            visible_button_1: true,
+            visible_button_2: false,
         }
          
     },
     methods:{ 
         hide(){
             this.visible =  true
+            this.visible_button_1 = false
+            this.visible_button_2 = true
         },
         hide_2(){
             this.visible = false
+            this.visible_button_2 = false
+            this.visible_button_1 = true
+
+
         }
+
     }
 }
 </script>
@@ -20,6 +29,6 @@ export default {
 
 <template>
     <p v-if="visible">text_1</p>
-    <button @click="hide" style="margin: 10px;">Появись</button>
-    <button @click="hide_2" style="margin: 10px;">Уберись</button>
+    <button @click="hide" v-if="visible_button_1">Появись</button>
+    <button @click="hide_2"  v-if="visible_button_2">Уберись</button>
 </template>
