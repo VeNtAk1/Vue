97d358d32ed5286a8f1c5ae213d96077fa128f08
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-25 12:28:53 +0300

    16_Vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script>
 export default {
-  data() {
-    return {
-		cost: 100,
-        amount: 2,
-	}
-  },
-  methods:{
-    update_cost(){
-        this.cost = 150
-    },
-    price: function(){
-            return this.amount * this.cost
+    methods:{  
+        first(){
+            alert("Gh")
         }
-    }   
+    }
 }
 </script>
 
 
 <template>
-    <button @click="update_cost">Поменять</button>
-    <p> {{ price() }}</p>
+    <a @click.stop>Нажми на меня</a>
+    <a @click.once="first">Нажми на меня_#2</a>
 </template>
