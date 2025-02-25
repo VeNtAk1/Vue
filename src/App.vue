8b87f2ab166b7da5b97bab29e10f02afe8ec471e
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-25 11:56:53 +0300

    12_Vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <script>
-import NewComponent from './NewComponent.vue'
-export default{
-    data() {
-	    return {
-		    num1: 1,
-		    num2: 2,
-	    };
+export default {
+  data() {
+    return {
+      inputNumber: null,  // Ввод пользователя
+      dayOfWeek: '',      // Результат для дня по числу
+      currentDay: '',     // Результат для текущего дня
+    };
+  },
+  methods: {
+    // Метод для получения дня недели по числу
+    getDayOfWeek(number) {
+      const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+      
+      if (number >= 1 && number <= 7) {
+        this.dayOfWeek = days[number - 1]; // Возвращаем день недели, уменьшая на 1
+      } else {
+        this.dayOfWeek = "Пожалуйста, введите число от 1 до 7.";
+      }
     },
-    methods: {
-    show: function(){
-        alert(this.num1 + this.num2)
-        }
-    },
-    mounted() {
-        this.show();
+    // Метод для получения текущего дня недели
+    getCurrentDay() {
+      const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+      const currentDayIndex = new Date().getDay(); // Метод getDay() возвращает индекс дня недели от 0 до 6
+      this.currentDay = days[currentDayIndex];
     }
+  },
+  mounted() {
+    this.getCurrentDay();  // Вызываем метод сразу после монтирования компонента, чтобы отобразить текущий день
+  }
 };
 </script>
 <template>
-
+    <div>
+      <input v-model.number="inputNumber" type="number" placeholder="Введите число от 1 до 7">
+      <button @click="getDayOfWeek(inputNumber)">
+        Получить день недели
+      </button>
+      <p v-if="dayOfWeek">День недели по номеру: {{ dayOfWeek }}</p>
+      <p>Сегодняшний день недели: {{ currentDay }}</p>
+    </div>
 </template>
