4d0a2792ed9299a440e44c9a5621dad0e79631f4
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-25 12:02:59 +0300

    13_Vue

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,35 @@ export default {
   data() {
     return {
       inputNumber: null,  // Ввод пользователя
-      dayOfWeek: '',      // Результат для дня по числу
-      currentDay: '',     // Результат для текущего дня
     };
   },
   methods: {
-    // Метод для получения дня недели по числу
-    getDayOfWeek(number) {
-      const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-      
-      if (number >= 1 && number <= 7) {
-        this.dayOfWeek = days[number - 1]; // Возвращаем день недели, уменьшая на 1
+    // Метод для вычисления квадрата числа
+    showSquare(number) {
+      if (number || number === 0) { // Проверяем, что введено число
+        alert(`Квадрат числа равен ${number * number}`);
       } else {
-        this.dayOfWeek = "Пожалуйста, введите число от 1 до 7.";
+        alert("Пожалуйста, введите число.");
       }
-    },
-    // Метод для получения текущего дня недели
-    getCurrentDay() {
-      const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-      const currentDayIndex = new Date().getDay(); // Метод getDay() возвращает индекс дня недели от 0 до 6
-      this.currentDay = days[currentDayIndex];
     }
-  },
-  mounted() {
-    this.getCurrentDay();  // Вызываем метод сразу после монтирования компонента, чтобы отобразить текущий день
   }
 };
 </script>
+
 <template>
-    <div>
-      <input v-model.number="inputNumber" type="number" placeholder="Введите число от 1 до 7">
-      <button @click="getDayOfWeek(inputNumber)">
-        Получить день недели
-      </button>
-      <p v-if="dayOfWeek">День недели по номеру: {{ dayOfWeek }}</p>
-      <p>Сегодняшний день недели: {{ currentDay }}</p>
-    </div>
+  <div>
+    <input v-model.number="inputNumber" type="number" placeholder="Введите число">
+    <br>
+    <button @click="showSquare(inputNumber)">
+      Показать квадрат числа
+    </button>
+    <br>
+    <button @click="showSquare(2)">
+      Показать квадрат числа
+    </button>
+    <br>
+    <button @click="showSquare(3)">
+      Показать квадрат числа 3
+    </button>
+  </div>
 </template>
