c1f9cdc4988087ebfc190c006b3063e07cf101c4
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-25 12:12:42 +0300

    14_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,31 @@
 export default {
   data() {
     return {
-      inputNumber: null,  // Ввод пользователя
-    };
+		text: 'xxx',
+	}
   },
-  methods: {
-    // Метод для вычисления квадрата числа
-    showSquare(number) {
-      if (number || number === 0) { // Проверяем, что введено число
-        alert(`Квадрат числа равен ${number * number}`);
-      } else {
-        alert("Пожалуйста, введите число.");
-      }
+  methods:{
+    change(){
+        this.text = "ууу"
+    },
+    update_1: function(){
+        this.text = "ppp"
+    },
+    update_2(){
+        this.text = "eee"
     }
   }
 };
 </script>
 
+
 <template>
-  <div>
-    <input v-model.number="inputNumber" type="number" placeholder="Введите число">
-    <br>
-    <button @click="showSquare(inputNumber)">
-      Показать квадрат числа
-    </button>
-    <br>
-    <button @click="showSquare(2)">
-      Показать квадрат числа
-    </button>
-    <br>
-    <button @click="showSquare(3)">
-      Показать квадрат числа 3
-    </button>
-  </div>
+  {{ text }}
+  <br>
+  <button @click="change">Поменять на ууу</button>
+  <br>
+  <button @click="update_1" style="margin-top: 10px;">Поменять на ррр</button>
+  <br>
+  <button @click="update_2" style="margin-top: 10px;">Поменять на еее</button>
+
 </template>
