92bf2ab2fa41d6ceecf647063f4dafe9baab2890
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-28 11:20:19 +0300

    37_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,19 @@
 export default {
     data() {
         return {
-            arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            cssClasses: 'active valid',
         }
     },
     methods:{
-       show(){
-        this.arr.push(11)
-       },
-       firstdel(){
-        this.arr.shift()
-       }
+       
     }
 }
 </script>
 
 
 <template>
-    <p>3rgr</p>
+    <p :class="cssClasses">text</p>
 </template>
 <style>
-    p {
-        color: green;
-    }
+    
 </style>
