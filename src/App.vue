34a5db3f0b0e2eb1ac9e3d1766faaee20c8adc13
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-28 10:27:38 +0300

    28_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 export default {
     data() {
         return {
-            arr: [1, 2, 3, 4, 5]
+            arr: ['x', 'y', 'z'],
         }
     },
     methods:{
@@ -13,9 +13,8 @@ export default {
 
 
 <template>
-    <!-- <p v-for="elem in arr">{{ elem }} </p> -->
-    <!-- <p v-for="elem in arr">{{ elem**2 }} </p> -->
     <ul>
-        <li v-for="elem in arr">{{ elem }}</li>
+        <li  v-for="(elem, key) in arr">{{ key }}</li>
     </ul>
+
 </template>
