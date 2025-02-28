bfe4e9a2da20c0d50f727e053628f0754b18b80b
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-28 10:00:52 +0300

    23_Vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script>
 export default {
-    data(){
-        return{
-            isAuth: true, 
+    data() {
+        return {
+            day: "Пятница", // пусть сейчас там 3
         }
     },
     methods:{
@@ -13,6 +13,11 @@ export default {
 
 
 <template>
-    <p v-if="isAuth">+++</p>
-	<p v-else>---</p>
+    <p v-if="day === 'Понедельник' ">Понедельник</p>
+    <p v-if="day === 'Вторник' ">Вторник</p>
+    <p v-if="day === 'Среда' ">Среда</p>
+    <p v-if="day === 'Четверг' ">Четверг</p>
+    <p v-if="day === 'Пятница' ">Пятница</p>
+    <p v-if="day === 'Суббота' ">Суббота</p>
+    <p v-if="day === 'Воскресенье' ">Воскресенье</p>
 </template>
