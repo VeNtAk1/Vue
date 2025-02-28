9792f67d629101cbe427d955a2c2014748128e88
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-28 10:16:58 +0300

    25_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
 export default {
     data() {
         return {
-            age: 25, 
+            binar: false,
         }
     },
     methods:{
-       
-    },
+        show(){
+            this.binar = !this.binar
+        }
+    }
 }
 </script>
 
 
 <template>
-   <p v-if=" age < 18">Подросток</p>
-   <p v-if=" 19 < age < 25">Молодой человек</p>
-   <p v-if=" age > 26">Мужчина</p>
+    <div v-if="binar">
+        <p>+</p>
+        <p>+</p>
+        <p>+</p>
+    </div>
+   <button @click="show">Нажми на меня</button>
 </template>
