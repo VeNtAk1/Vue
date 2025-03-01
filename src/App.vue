afe4a4dff154fb2f42b55d8f82c8b9d9a7bd9f69
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-01 10:34:03 +0300

    65_Vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
     <div>
-      <h2>Список работников</h2>
-      <!-- Цикл v-for для создания компонента Employee для каждого работника -->
-      <employee
-        v-for="user in users"
-        :key="user.id"
-        :name="user.name"
-        :salary="user.salary"
-        :age="user.age"
+      <h2>Вызов функций из родительского компонента</h2>
+      <!-- Передаем две функции в дочерний компонент -->
+      <employee 
+        :firstFunction="firstFunction" 
+        :secondFunction="secondFunction"
       ></employee>
     </div>
   </template>
@@ -19,29 +16,14 @@
     components: {
       Employee // Регистрируем дочерний компонент
     },
-    data() {
-      return {
-        // Массив работников
-        users: [
-          {
-            id: 1,
-            name: 'name1',
-            salary: 100,
-            age: 30,
-          },
-          {
-            id: 2,
-            name: 'name2',
-            salary: 200,
-            age: 40,
-          },
-          {
-            id: 3,
-            name: 'name3',
-            salary: 300,
-            age: 50,
-          }
-        ]
+    methods: {
+      // Первая функция
+      firstFunction() {
+        alert('Первая функция вызвана!');
+      },
+      // Вторая функция
+      secondFunction() {
+        alert('Вторая функция вызвана!');
       }
     }
   }
