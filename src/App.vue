d91c6c06e7172c74d01373692f52d89bb4b9e25f
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-01 10:21:24 +0300

    50_2_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,52 @@
 export default {
     data() {
         return {
-            selectedDay: '', // Переменная для хранения выбранного дня
-            days: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'] // Список дней недели
+            selectedDay: '',
+            selectedMonth: '',
+            selectedYear: '',
+            days: Array.from({ length: 31 }, (_, i) => i + 1), // Список дней 1-31
+            months: [
+                'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 
+                'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+            ],
+            years: Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i) // Последние 100 лет
         }
     }
 }
 </script>
 
 <template>  
-    <p>Выберите день недели:</p>
+    <p>Выберите дату:</p>
 
-    <select v-model="selectedDay">
-        <option disabled value="">-- Выберите день --</option>
-        <option v-for="day in days" :key="day" :value="day">{{ day }}</option>
-    </select>
+    <label>День:
+        <select v-model="selectedDay">
+            <option disabled value="">-- День --</option>
+            <option v-for="day in days" :key="day" :value="day">{{ day }}</option>
+        </select>
+    </label>
 
-    <p v-if="selectedDay">Вы выбрали: {{ selectedDay }}</p>
+    <label>Месяц:
+        <select v-model="selectedMonth">
+            <option disabled value="">-- Месяц --</option>
+            <option v-for="month in months" :key="month" :value="month">{{ month }}</option>
+        </select>
+    </label>
+
+    <label>Год:
+        <select v-model="selectedYear">
+            <option disabled value="">-- Год --</option>
+            <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
+        </select>
+    </label>
+
+    <p v-if="selectedDay && selectedMonth && selectedYear">
+        Вы выбрали: {{ selectedDay }} {{ selectedMonth }} {{ selectedYear }} года
+    </p>
 </template>
 
 <style>
     select {
         display: block;
-        margin-top: 10px;
+        margin: 5px 0;
     }
 </style>
