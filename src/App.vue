12bcc3e09f5c12dc1ac42a098008b7d45458acb9
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-01 10:19:16 +0300

    48_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
 export default {
     data() {
         return {
-            selectedLanguages: [], // Массив для хранения выбранных языков
-            languages: ['JavaScript', 'Python', 'C++', 'Java', 'PHP'] // Список языков
+            selectedLanguage: '', // Переменная для хранения выбранного языка
+            languages: ['Русский', 'Английский', 'Французский', 'Немецкий', 'Испанский'] // Список языков
         }
     }
 }
 </script>
 
 <template>  
-    <p>Какие языки программирования вы знаете?</p>
-    
+    <p>Какой язык для вас родной?</p>
+
     <label v-for="lang in languages" :key="lang">
-        <input type="checkbox" v-model="selectedLanguages" :value="lang"> {{ lang }}
+        <input type="radio" v-model="selectedLanguage" :value="lang"> {{ lang }}
     </label>
 
-    <p v-if="selectedLanguages.length">Вы знаете: {{ selectedLanguages.join(', ') }}</p>
+    <p v-if="selectedLanguage">Ваш родной язык: {{ selectedLanguage }}</p>
 </template>
 
 <style>
