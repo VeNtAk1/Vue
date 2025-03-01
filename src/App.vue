1ac1ff6052ac0a4c86972b7f3b3383985cb66635
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-01 10:27:35 +0300

    56_Vue

--- src/App.vue
+++ src/App.vue
@@ -22,11 +22,29 @@ export default {
                     age: 50,
                 },
             ],
+            editingUser: null, // Переменная для хранения редактируемого работника
+            updatedName: '',
+            updatedSalary: '',
+            updatedAge: ''
         }
     },
     methods: {
         removeUser(id) {
             this.users = this.users.filter(user => user.id !== id); // Удаляем работника по id
+        },
+        editUser(user) {
+            this.editingUser = user;
+            this.updatedName = user.name;
+            this.updatedSalary = user.salary;
+            this.updatedAge = user.age;
+        },
+        saveUser() {
+            if (this.editingUser) {
+                this.editingUser.name = this.updatedName;
+                this.editingUser.salary = this.updatedSalary;
+                this.editingUser.age = this.updatedAge;
+                this.editingUser = null; // Закрываем форму редактирования
+            }
         }
     }
 }
@@ -40,6 +58,7 @@ export default {
                 <th>Зарплата</th>
                 <th>Возраст</th>
                 <th>Удалить</th>
+                <th>Редактировать</th>
             </tr>
         </thead>
         <tbody>
@@ -48,11 +67,34 @@ export default {
                 <td>{{ user.salary }}</td>
                 <td>{{ user.age }}</td>
                 <td>
-                    <a href="#" @click.prevent="removeUser(user.id)">Удалить</a> <!-- При клике удаляется работник -->
+                    <a href="#" @click.prevent="removeUser(user.id)">Удалить</a>
+                </td>
+                <td>
+                    <a href="#" @click.prevent="editUser(user)">Редактировать</a>
                 </td>
             </tr>
         </tbody>
     </table>
+
+    <!-- Форма редактирования -->
+    <div v-if="editingUser">
+        <h3>Редактирование пользователя</h3>
+        <form @submit.prevent="saveUser">
+            <div>
+                <label for="name">Имя:</label>
+                <input type="text" id="name" v-model="updatedName">
+            </div>
+            <div>
+                <label for="salary">Зарплата:</label>
+                <input type="number" id="salary" v-model="updatedSalary">
+            </div>
+            <div>
+                <label for="age">Возраст:</label>
+                <input type="number" id="age" v-model="updatedAge">
+            </div>
+            <button type="submit">Сохранить изменения</button>
+        </form>
+    </div>
 </template>
 
 <style>
@@ -65,10 +107,16 @@ export default {
         text-align: left;
     }
     a {
-        color: red;
+        color: blue;
         cursor: pointer;
     }
     a:hover {
         text-decoration: underline;
+    }
+    form {
+        margin-top: 20px;
+    }
+    form div {
+        margin-bottom: 10px;
     }
 </style>
