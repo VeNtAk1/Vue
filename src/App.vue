6c0aec13f2da0b2c8d1077395529fa6e50a49f4e
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-01 10:41:21 +0300

    70_Vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,59 @@
 <template>
     <div>
-      <p>Имя: {{ user.name }}</p>
-      <p>Зарплата: {{ user.salary }}</p>
-      <p>Возраст: {{ user.age }}</p>
-      <!-- Кнопка для удаления работника -->
-      <button @click="deleteEmployee">Удалить работника</button>
+      <template v-if="!isEdit">
+        <!-- Режим отображения данных -->
+        <p>{{ user.name }} {{ user.surname }}</p>
+        <button @click="edit">Редактировать</button>
+      </template>
+      <template v-else>
+        <!-- Режим редактирования -->
+        <input v-model="newName" />
+        <input v-model="newSurname" />
+        <button @click="save">Сохранить</button>
+      </template>
     </div>
   </template>
   
   <script>
   export default {
     props: {
-      // Пропс для получения данных работника
       user: {
         type: Object,
-        required: true
+        required: true,
       },
-      // Пропс для метода удаления работника
-      removeUser: {
-        type: Function,
-        required: true
-      }
+    },
+    data() {
+      return {
+        isEdit: false, // Флаг редактирования
+        newName: this.user.name, // Новый name
+        newSurname: this.user.surname, // Новый surname
+      };
     },
     methods: {
-      // Метод для вызова удаления работника
-      deleteEmployee() {
-        // Передаем id работника для удаления
-        this.removeUser(this.user.id);
-      }
-    }
+      edit() {
+        // Включаем режим редактирования
+        this.isEdit = true;
+      },
+      save() {
+        // Сохраняем изменения и возвращаемся в режим отображения
+        this.isEdit = false;
+        const updatedUser = {
+          ...this.user,
+          name: this.newName,
+          surname: this.newSurname,
+        };
+        this.$emit('updateUser', updatedUser); // Отправляем обновленные данные в родительский компонент
+      },
+    },
   };
   </script>
   
   <style scoped>
+  input {
+    margin-right: 5px;
+  }
   button {
     margin-top: 10px;
-    padding: 5px 10px;
-    cursor: pointer;
   }
   </style>
   
