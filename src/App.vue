b66d2ea9170471fe0e6ca6228e4f18b8421a749e
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-01 10:24:41 +0300

    52_3_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
 export default {
     data() {
         return {
-            message: '' // Текст, который будет отображаться
+            items: ['Первый элемент', 'Второй элемент', 'Третий элемент'], // Исходный массив
+            inputText: '' // Текст, введённый в инпут
         }
     },
     methods: {
-        handleClick(event) {
-            if (event.ctrlKey) { // Проверяем, зажат ли Ctrl
-                event.preventDefault(); // Предотвращаем переход по ссылке
-                this.message = 'Вы нажали на ссылку с зажатым Ctrl!';
+        addItem() {
+            if (this.inputText.trim() !== '') { // Проверяем, не пустой ли введённый текст
+                this.items.push(this.inputText); // Добавляем новый элемент в массив
+                this.inputText = ''; // Очищаем инпут после добавления
             }
         }
     }
@@ -17,16 +18,23 @@ export default {
 </script>
 
 <template>  
-    <a href="https://example.com" @click="handleClick">Нажмите на меня с Ctrl</a>
-    <p v-if="message">{{ message }}</p>
+    <ul>
+        <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+
+    <input type="text" v-model="inputText" placeholder="Введите текст">
+    <button @click="addItem">Добавить в список</button>
 </template>
 
 <style>
-    a {
-        display: block;
-        margin-bottom: 10px;
-        color: blue;
-        cursor: pointer;
-        text-decoration: underline;
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        margin: 5px 0;
+    }
+    input {
+        margin-right: 10px;
     }
 </style>
