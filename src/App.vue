7a45bc265018a60f60f835c150c9defebb7e6960
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-01 10:22:17 +0300

    51_Vue

--- src/App.vue
+++ src/App.vue
@@ -2,52 +2,28 @@
 export default {
     data() {
         return {
-            selectedDay: '',
-            selectedMonth: '',
-            selectedYear: '',
-            days: Array.from({ length: 31 }, (_, i) => i + 1), // Список дней 1-31
-            months: [
-                'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 
-                'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
-            ],
-            years: Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i) // Последние 100 лет
+            isDisabled: false // Флаг блокировки инпута
+        }
+    },
+    methods: {
+        toggleInput() {
+            this.isDisabled = !this.isDisabled; // Меняем состояние блокировки
         }
     }
 }
 </script>
 
 <template>  
-    <p>Выберите дату:</p>
-
-    <label>День:
-        <select v-model="selectedDay">
-            <option disabled value="">-- День --</option>
-            <option v-for="day in days" :key="day" :value="day">{{ day }}</option>
-        </select>
-    </label>
-
-    <label>Месяц:
-        <select v-model="selectedMonth">
-            <option disabled value="">-- Месяц --</option>
-            <option v-for="month in months" :key="month" :value="month">{{ month }}</option>
-        </select>
-    </label>
-
-    <label>Год:
-        <select v-model="selectedYear">
-            <option disabled value="">-- Год --</option>
-            <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
-        </select>
-    </label>
-
-    <p v-if="selectedDay && selectedMonth && selectedYear">
-        Вы выбрали: {{ selectedDay }} {{ selectedMonth }} {{ selectedYear }} года
-    </p>
+    <input type="text" :disabled="isDisabled" placeholder="Введите текст...">
+    <button @click="toggleInput">
+        {{ isDisabled ? 'Разблокировать' : 'Заблокировать' }}
+    </button>
 </template>
 
 <style>
-    select {
+    input {
         display: block;
-        margin: 5px 0;
+        margin-bottom: 10px;
+        padding: 5px;
     }
 </style>
